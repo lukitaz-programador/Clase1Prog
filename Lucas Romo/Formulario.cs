128aab68d1eb039c1e19323bd1c7176fad23c420
lukitaz-programador <lucasromo.personal@gmail.com>
2025-08-19 13:16:33 -0300

    Agregar archivos de proyecto.

---
+++ Lucas Romo/Formulario.cs
@@ -0,0 +1,15 @@
+namespace Lucas_Romo
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
